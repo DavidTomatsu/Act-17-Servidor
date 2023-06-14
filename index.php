281052635b4331434e75a00347557514a4814c27
DavidTomatsu <125592543+DavidTomatsu@users.noreply.github.com>
2023-06-14 08:24:50 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+        echo '<p> Valdes Tomatsu David 😎 </p>'; 
+        echo '<p> #2500 Zona centro</p>';
+        echo '<p> Colonia Hidalgo</p>';
+        echo '<p> Municipio Saltillo, Coah </p>';
+        echo '<p> Telefono 844 414 8902 </p>';
+    ?> 
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,8 @@
+1. Inicio
+2. Mostrar nombre
+3. Mostrar calle
+4. Mostrar casa
+5. Mostrar Colonia
+6. Mostrar Municipio
+7. Mostrar numero de telefono
+8. Fin
